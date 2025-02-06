d41f25bd3cc31fe72ac45ff7a6f283e3c99a8706
Tami Sweitzer <tamara0715@gmail.com>
2025-02-06 14:10:08 -0600

    Add text to About page.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
-    <x-slot:heading>About</x-slot:heading>
+    <div class="px-4">
+        <x-slot:heading>About</x-slot:heading>
+        <p>This is a test app, following the <a href="https://laracasts.com/series/30-days-to-learn-laravel-11">30 Days
+                to Learn Laravel</a> at Laracasts.</p>
+    </div>
 </x-layout>
