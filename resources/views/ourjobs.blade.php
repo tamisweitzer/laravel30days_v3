ef2b28e12b1caa5f6b4d800984f5796ef357bbd1
Tami Sweitzer <tamara0715@gmail.com>
2025-02-06 13:47:50 -0600

    Update styling in OurJobs View. Add hover effect.

--- resources/views/ourjobs.blade.php
+++ resources/views/ourjobs.blade.php
@@ -1,10 +1,10 @@
 <x-layout>
     <x-slot:heading>Jobs</x-slot:heading>
 
-    <ul>
+    <ul class="mt-8 mb-8">
         @foreach ($jobs as $job)
-            <li class="mb-2">
-                <a href="/ourjobs/{{ $job['id'] }}">
+            <li class="border border-t-0 border-l-0 border-r-0 hover:bg-gray-200">
+                <a href="/ourjobs/{{ $job['id'] }}" class="pt-4 pb-4 block">
                     <strong>{{ $job['title'] }}</strong>: pays {{ $job['salary'] }} per year.
                 </a>
             </li>
