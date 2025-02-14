b7fb6d10d8333402d2b1e2253ad9c711ad4b6f99
Tami Sweitzer <tamara0715@gmail.com>
2025-02-14 00:56:39 -0600

    Add a Create Job button in header of pages. Refactor button into a component.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -127,8 +127,9 @@
         </nav>
 
         <header class="bg-white shadow">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex justify-between">
                 <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
+                <x-button href="/ourjobs/create">Create Job</x-button>
             </div>
         </header>
         <main>
