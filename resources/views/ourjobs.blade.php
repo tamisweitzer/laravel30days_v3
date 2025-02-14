9a684c92abe97c40061d29b58e41a54adfb3a202
Tami Sweitzer <tamara0715@gmail.com>
2025-02-13 21:30:41 -0600

    Continue reformatting style of Jobs listing page. Add employer name above job title. Change the bolding on the job name from a strong tag to a span tag with bold class so that it is more semantic.

--- resources/views/ourjobs.blade.php
+++ resources/views/ourjobs.blade.php
@@ -3,7 +3,13 @@
     <div class="space-y-4">
         @foreach ($jobs as $job)
             <a href="/ourjobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded ">
-                <strong>{{ $job['title'] }}</strong>: pays {{ $job['salary'] }} per year.
+                <div class="text-bold text-blue-500">
+                    {{ $job->employer->name }}
+                </div>
+
+                <div>
+                    <span class="text-bold">{{ $job['title'] }}</span>: pays {{ $job['salary'] }} per year.
+                </div>
             </a>
         @endforeach
     </div>
