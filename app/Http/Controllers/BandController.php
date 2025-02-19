c996fb11de6db8657b3180b39d012dbf08be3014
Tami Sweitzer <tamara0715@gmail.com>
2025-02-18 21:44:13 -0600

    Working on bands and others.

--- app/Http/Controllers/BandController.php
+++ app/Http/Controllers/BandController.php
@@ -18,21 +18,4 @@ class BandController extends Controller {
         $band = Band::find($id);
         return view('bands.show', ['band' => $band]);
     }
-
-
-    public function insertMany() {
-        DB::table('bands')->insertGetId(
-            [
-                'name' => 'Annex',
-                'fullname' => 'Annex',
-                'excerpt' => "Walk into an annex show and get smacked with a wall of sound. This is a rock band from Green Bay. The Marshalls are dialed high, the Les Pauls are cranked!",
-                'bio' => "annex bio here ",
-                'city_id' => 3,
-                'state_id' => 1,
-                'website_url' => 'x',
-                'thumbnail_img' => 'ur;',
-                'banner_img' => 'xx'
-            ],
-        );
-    }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -96,14 +96,7 @@
                     <x-nav-link-mobile href="/bands" :active="request()->is('bands')">Bands</x-nav-link-mobile>
                     <x-nav-link-mobile href="/cities" :active="request()->is('cities')">Cities</x-nav-link-mobile>
                     <x-nav-link-mobile href="/events" :active="request()->is('events')">Events</x-nav-link-mobile>
-                    <x-nav-link-mobile href="/test1" :active="request()->is('test1')">Test1</x-nav-link-mobile>
-                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                    {{-- <a href="/" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-                        aria-current="page">Home</a>
-                    <a href="/about"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                    <a href="/contact"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a> --}}
+                    <x-nav-link-mobile href="/venues" :active="request()->is('venues')">Venues</x-nav-link-mobile>
                 </div>
                 <div class="border-t border-gray-700 pb-3 pt-4">
                     <div class="flex items-center px-5">
