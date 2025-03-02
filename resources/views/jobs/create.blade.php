b9fb346624a443a895c00bb95b5f2db0c3df1294
Tami Sweitzer <tamara0715@gmail.com>
2025-03-01 21:11:30 -0600

    Add padding to the Submit button in the Create Job form. Start using the default button component for the Cancel button.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -39,9 +39,9 @@
         </div>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
+            <x-button>Cancel</x-button>
             <button type="submit"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                class="rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
         </div>
     </form>
 </x-layout>
