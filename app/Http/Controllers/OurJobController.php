a437f834ed9c21902ea7349b91564fb7f88fe05d
Tami Sweitzer <tamara0715@gmail.com>
2025-03-01 19:39:30 -0600

    Insert new jobs into database by using the auto-generated ID field so that adding new jobs via the "Create Job"  button will work.

--- app/Http/Controllers/OurJobController.php
+++ app/Http/Controllers/OurJobController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\OurJob;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OurJobController extends Controller {
     public function index() {
         // Eager load jobs with the associated employers.
-        $jobs = OurJob::with('employer')->paginate(20);
+        $jobs = OurJob::with('employer')->orderBy('id', 'desc')->paginate(20);
         return view('jobs.index', ['jobs' => $jobs]);
     }
 
@@ -22,7 +23,7 @@ class OurJobController extends Controller {
     }
 
     public function store() {
-        OurJob::create([
+        DB::table('our_jobs')->insertGetId([
             'title' => request("title"),
             'salary' => request("salary"),
             'employer_id' => 1,
