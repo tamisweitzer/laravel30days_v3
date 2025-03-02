b0de442a47930723b08dedf9b6460f303a27693d
Tami Sweitzer <tamara0715@gmail.com>
2025-03-01 19:05:56 -0600

    Playing with styling of list of events. Create component for the inline links.

---
+++ resources/views/components/link-inline.blade.php
@@ -0,0 +1,2 @@
+<a
+    {{ $attributes->merge(['class' => 'underline underline-offset-4 decoration-orange-100 hover:decoration-orange-500 hover:text-orange-600']) }}>{{ $slot }}</a>
--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,21 +1,25 @@
-<x-layout>
+<x-layout class="">
     <x-slot:heading>Events</x-slot:heading>
-    {{-- {{ dd($events) }} --}}
-    <ul>
+    <ul class="text-slate-500">
         @foreach ($events as $event)
             {{-- {{ dd($event) }} --}}
-            <li class="p-4 mb-2 bg-slate-50 border b-slate-100">
-                <h2 class="mb-2 text-lg">
-                    <a href="/bands/{{ $event->band->id }}"
-                        class="border-b border-dotted border-gray-300 hover:bg-gray-300">{{ $event->band->name }}</a>
-                    at
-                    <a href="venues/{{ $event->venue->id }}">{{ $event->venue->name }}</a>
+            <li class="p-4 mb-2 bg-slate-50 shadow-inner border ">
+
+                <h2 class="mb-2 text-slate-900 font-bold tracking-widest">
+                    <x-link-inline href="/bands/{{ $event->band->id }}"
+                        class="text-2xl">{{ $event->band->name }}</x-link-inline>
                 </h2>
 
-                <div class="text-sm">
-                    <a href="cities/{{ $event->band->city->id }}" class="">{{ $event->band->city->name }}</a>,
-                    <a href="events/{{ date('Y/m', strtotime($event->event_time)) }}"
-                        class="">{{ date('m/d/Y', strtotime($event->event_time)) }}</a>
+                <div class="text-base mb-1">
+                    <x-link-inline href="venues/{{ $event->venue->id }}"
+                        class="text-lg">{{ $event->venue->name }}</x-link-inline>
+                    <span> in </span>
+                    <x-link-inline href="cities/{{ $event->band->city->id }}"
+                        class="text-lg">{{ $event->band->city->name }}</x-link-inline>
+                </div>
+
+                <div class="text-xs">
+                    {{ date('M d, Y', strtotime($event->event_time)) }}
                     at 8:00 pm
                 </div>
 
