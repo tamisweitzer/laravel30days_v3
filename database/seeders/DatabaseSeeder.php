f922da036ab679978dd3c0aeb009090cf44596e1
Tami Sweitzer <tamara0715@gmail.com>
2025-03-01 20:33:34 -0600

    Correct the function to call the job seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder {
             'email' => 'test@example.com',
         ]);
 
-        OurJob::factory(10)->create();
+        // Call the job seeder.
+        $this->call(OurJobSeeder::class);
     }
 }
