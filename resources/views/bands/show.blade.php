9e5af7de67ffde19c291772b36839da5f47be146
Tami Sweitzer <tamara0715@gmail.com>
2025-12-14 21:13:18 -0600

    Make each event clickable on the band profile page.

--- resources/views/bands/show.blade.php
+++ resources/views/bands/show.blade.php
@@ -9,17 +9,21 @@
 
     <h2 class="text-2xl mt-8 mb-4">Upcoming Shows</h2>
 
+    {{-- @dd($events) --}}
+
     @if (!$events->isEmpty())
         <div class="grid sm:grid-cols-2 md:grid-cols-3 gap-2">
             @foreach ($events as $event)
-                <div class="p-4 bg-slate-50 border b-slate-100">
-                    <div class="text-xl">{{ $event->venue->name }}</div>
-                    <div class="text-sm text-gray-600">{{ $event->venue_name }}</div>
-                    <div class="text-sm text-gray-600">
-                        {{ $event->event_date }} -
-                        {{ $event->event_time }}
+                <a href="/events/{{ $event->id }}">
+                    <div class="p-4 bg-slate-50 border b-slate-100">
+                        <div class="text-xl">{{ $event->venue->name }}</div>
+                        <div class="text-sm text-gray-600">{{ $event->venue_name }}</div>
+                        <div class="text-sm text-gray-600">
+                            {{ $event->event_date }} -
+                            {{ $event->event_time }}
+                        </div>
                     </div>
-                </div>
+                </a>
             @endforeach
         </div>
     @else
