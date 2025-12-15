b4272db21f02eb065828aa3ed7f79f52a4e1647f
Tami Sweitzer <tamara0715@gmail.com>
2025-12-14 21:48:13 -0600

    Change page title.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>My Laravel Playground</title>
     {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
     <!-- Styles / Scripts -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
