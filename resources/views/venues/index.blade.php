6945dc48eeba6dc0a03957ede45ced4b04683f50
Tami Sweitzer <tamara0715@gmail.com>
2025-12-14 22:01:09 -0600

    Format list of venues in a grid.

--- resources/views/venues/index.blade.php
+++ resources/views/venues/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-slot:heading>Venues</x-slot:heading>
 
-    <ul>
+    <ul class="grid sm:grid-cols-2 md:grid-cols-3 gap-2">
         @foreach ($venues as $venue)
             <li class="p-4 mb-2 bg-slate-50 border b-slate-100">
                 <a href="/cities/{{ $venue->id }}">
