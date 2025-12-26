eed3926fc455ed33e6fced22fd474c3598ee1884
Tami Sweitzer <tamara0715@gmail.com>
2025-12-25 22:49:32 -0600

    Start working on a media model for saving the image path to the DB.

---
+++ app/Models/Media.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Media extends Model
+{
+    protected $fillable = [
+            'file_name',
+            'mime_type',
+            'file_size',
+            'path',
+
+    ]
+}
